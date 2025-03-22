84c5f68a364b669cde3b81d7a78aa5d5581ec836
aallali <contact@allali.me>
2025-03-22 23:06:58 +0000

    test: Disable stdout logs in `Jest`

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,12 @@
+// eslint-disable-next-line no-undef
 module.exports = {
 	preset: 'ts-jest',
 	testEnvironment: 'node',
 	rootDir: '.',
 	coverageDirectory: "coverage",  // Directory to save coverage reports
 	coverageReporters: ["json", "lcov", "text", "clover"],
+	silent: true,
+	verbose: false,
 	collectCoverageFrom: [
 		"src/**/*.ts",
 		"!src/test.ts",
