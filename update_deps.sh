7cbb7fc022c0d0fa00632496a813329d19a78ace
aallali <contact@allali.me>
2025-03-22 23:08:12 +0000

    build: Refact `update_deps` script to detect package manager

--- update_deps.sh
+++ update_deps.sh
@@ -10,20 +10,30 @@ fi
 # Identify lock file type
 if [ -f "yarn.lock" ]; then
     LOCK_FILE="yarn.lock"
+    NPM="yarn"
 elif [ -f "pnpm-lock.yaml" ]; then
     LOCK_FILE="pnpm-lock.yaml"
+    NPM="pnpm"
 else
     LOCK_FILE="package-lock.json"
+    NPM="npm"
 fi
 
 for package in $(echo "$outdated" | jq -r 'keys[]'); do
-
     version=$(echo "$outdated" | jq -r ".\"$package\".latest")
+    
+    # Skip prerelease versions (containing a hyphen) (e.g. "2.1.0-alpha", "2.1.0-beta", "2.1.0-rc.1")
+    if [[ "$version" == *"-"* ]]; then
+        echo "Skipping $package@$version as it appears to be a prerelease version"
+        continue
+    fi
+    
     echo "Updating $package to version $version..."
-    pnpm install "$package@$version"
+    $NPM install "$package@$version"
     # Commit the update
     git add package.json $LOCK_FILE
     git commit -m "chore(deps): Update \`$package\` to \`$version\`"
+    echo "chore(deps): Update \`$package\` to \`$version\`"
 done
 
 echo "All outdated packages have been updated and committed."
